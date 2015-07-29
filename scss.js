a33d1785379f8b6b5a0f5cff7f2c823c2453201b
inuscript <inuscript@gmail.com>
2015-07-30 08:49:12 +0900

    to module

--- scss.js
+++ scss.js
@@ -1,6 +1,6 @@
 var jsonSass = require("json-sass")
 
-var getSassContent = function(map, fontVariable, asDefault){
+module.exports = function(map, fontVariable, asDefault){
   var prefix = "$" + fontVariable + ": "
   var suffix = asDefault ? " !default;" : ""
   var scss = prefix + jsonSass.convertJs(map) + suffix
