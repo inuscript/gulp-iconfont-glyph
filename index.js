708c5b50a2dcb86844f9dc23cc870de2d31ca922
inuscript <inuscript@gmail.com>
2015-07-30 23:22:41 +0900

    quote and backslash option

--- index.js
+++ index.js
@@ -4,38 +4,40 @@ var through2 = require("through2")
 var plexer = require("plexer")
 var svgicons2svgfont = require('gulp-svgicons2svgfont')
 var gutil = require("gulp-util")
-
+var quote = require("quote")
+var empty = function(s){ return s }
 var PLUGIN_NAME = "iconfont-glyph"
 
 var svgStream = function(options){
-  var svgOption = extend({}, options) // copy
-  svgOption.log = function(){}
-  return svgicons2svgfont(svgOption)
+  options.log = function(){}
+  return svgicons2svgfont(options)
 }
 
-var glyphsMap = function(glyphs){
+var glyphsMap = function(glyphs, withQuote, withBackslash){
+  var bs = withBackslash ? "\\" : ""
+  var fn = withQuote ? quote : empty
+
   return glyphs.reduce(function(obj, glyph){
     glyphs.forEach(function(glyph){
-      var code = "\\" + glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase()
-      obj[glyph.name] = code
+      // var code = "\\" + glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase()
+      var code = glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase()
+      obj[glyph.name] = fn(bs + code)
     })
     return obj
   }, {})
 }
 
-var getMapValue = function(glyphs, appendMap){
-  var value = {
-    glyphs : glyphsMap(glyphs)
-  }
-  return extend(value, appendMap)
-}
 
 
 module.exports = function(opt){
-  var inputStream = svgStream(opt)
+  var svgOptions = extend({}, opt.svgOptions) // copy
+  var inputStream = svgStream(svgOptions)
   var outputStream = new stream.PassThrough({ objectMode: true });
   var _glyphs = undefined;
-
+  var options = extend({
+    withQuote: false, 
+    backslash: true
+  }, opt)
   inputStream.on('glyphs', function(glyphs){
     _glyphs = glyphs // memorize
   }).on('error', function(err){
@@ -44,12 +46,14 @@ module.exports = function(opt){
     if (_glyphs === undefined) {
       return cb(null);
     }
-    var mapValue = getMapValue(_glyphs)
+    var data = {
+      glyphs: glyphsMap(_glyphs, options.withQuote, options.withBackslash)
+    }
     var glyphFile = new gutil.File({
       path: gutil.replaceExtension(file.path),
       extension: "json",
-      contents: new Buffer(JSON.stringify(mapValue)),
-      data: mapValue // gulp-data compability
+      contents: new Buffer(JSON.stringify(data)),
+      data: data // gulp-data compability
     })
     outputStream.push(glyphFile)
     cb()
