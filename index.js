523b554d2203fcb96181482280230d4fc9679ffd
inuscript <inuscript@gmail.com>
2015-07-30 22:03:46 +0900

    Add raw-data

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ var stream = require("stream")
 var through2 = require("through2")
 var plexer = require("plexer")
 var svgicons2svgfont = require('gulp-svgicons2svgfont')
-var quote = require("quote")
 var gutil = require("gulp-util")
 
 var PLUGIN_NAME = "iconfont-glyph"
 
 var svgStream = function(options){
-  var svgOption = extend({}, options)
+  var svgOption = extend({}, options) // copy
   svgOption.log = function(){}
   return svgicons2svgfont(svgOption)
 }
@@ -42,17 +41,15 @@ module.exports = function(opt){
   }).on('error', function(err){
     new gutil.PluginError(PLUGIN_NAME, err, {showStack: true})
   }).pipe(through2.obj(function(file, enc, cb){
-    if (file.isNull() || _glyphs === undefined) {
+    if (_glyphs === undefined) {
       return cb(null);
-    }
-    if (file.isStream()) {
-      return cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
     }
     var mapValue = getMapValue(_glyphs)
     var glyphFile = new gutil.File({
       path: gutil.replaceExtension(file.path),
       extension: "json",
-      contents: new Buffer(JSON.stringify(mapValue))
+      contents: new Buffer(JSON.stringify(mapValue)),
+      data: mapValue // gulp-data compability
     })
     outputStream.push(glyphFile)
     cb()
